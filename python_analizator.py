d0e2aaa63c73afa1b06c59a01849325548788490
KarolinaAntonowicz <karola.antonowicz@gmail.com>
2019-12-08 19:15:17 +0100

    add download function

--- python_analizator.py
+++ python_analizator.py
@@ -1,6 +1,28 @@
-
+import requests
 
 menu = ['1. Pobierz plik z internetu', '2. Zlicz liczbę liter w pobranym pliku', '3. Zlicz liczbę wyrazów w pliku',
         '4. Zlicz liczbę znaków interpunkcyjnych w pliku.', '5. Zlicz liczbę zdań w pliku',
         '6. Wygeneruj raport o użyciu liter (A-Z)', '7. Zapisz statystyki z punktów 2-5 do pliku statystyki.txt',
         '8. Wyjście z programu']
+isWorking = True
+
+text = list()
+
+
+def download():
+    url = 'https://s3.zylowski.net/public/input/6.txt'
+    r = requests.get(url)
+    filename = url.split('/')[-1]
+    # zapisywanie pliku
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+
+    # otwieranie pliku
+    file = open(filename, 'r')
+    text = list(file)
+    # text = file.read()
+    file.close()
+    return text
+
+
+text = download()
